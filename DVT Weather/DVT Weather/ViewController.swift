18968ef69947a592a7f01aa5686255d3352ea66a
adex <biosecsolutionng@gmail.com>
2020-02-23 00:25:50 +0100

    set up project

---
+++ DVT Weather/DVT Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DVT Weather
+//
+//  Created by adex on 22/02/2020.
+//  Copyright © 2020 Gideon Oyediran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
